a250514d5bd5e8b015030af31b7b158f5992ce35
Shashank Gupta <shashankcm90@gmail.com>
2018-09-23 21:11:36 -0500

    Implemented Widget component to add widgets

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-redux-sagas-redux-forms-authentication
 React and Redux Sagas Authentication (J C Morrison)
+
+Reference from below link
+
+https://start.jcolemorrison.com/build-deploy-node-react-on-aws/
--- src/index-reducer.js
+++ src/index-reducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import client from './client/reducer';
 import signup from './signup/reducer';
-import login from './login/reducer'
+import login from './login/reducer';
+import widgets from './widgets/reducer';
 
 const IndexReducer = combineReducers({
     client,
     form,
     signup,
-    login
+    login,
+    widgets,
 })
 
 export default IndexReducer;
